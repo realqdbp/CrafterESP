14c72a76710edfde5e983c5b62a967d5e46a5e37
Joshua <me@qdbp.codes>
2023-07-03 00:26:57 +0200

    Rewrite of main.cpp. Updated gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include "LittleFS.h"
+using namespace std;
 
-std::unordered_map<int, bool> pinMap;
 
 #define D1_PIN 5 //Decke1
 #define D2_PIN 4 //Decke2
@@ -15,40 +15,99 @@ std::unordered_map<int, bool> pinMap;
 #define D7_PIN 13 //frei
 #define D8_PIN 15 //frei
 
-IPAddress ipv4(192, 168, 1, 180);
-IPAddress standardGateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
-IPAddress primaryDNS(1, 1, 1, 1);
-IPAddress secondaryDNS(8, 8, 8, 8);
+unordered_map<int, bool> pinStateMap;
 
 AsyncWebServer server(80);
+
+
+void setupPins(){
+    pinMode(D1_PIN, OUTPUT);
+    pinMode(D2_PIN, OUTPUT);
+    pinMode(D3_PIN, OUTPUT);
+    pinMode(D4_PIN, OUTPUT);
+    pinMode(D5_PIN, OUTPUT);
+    pinMode(D6_PIN, OUTPUT);
+    pinMode(D7_PIN, OUTPUT);
+    pinMode(D8_PIN, OUTPUT);
+
+    pinStateMap[D1_PIN] = false;
+    pinStateMap[D2_PIN] = false;
+    pinStateMap[D3_PIN] = false;
+    pinStateMap[D4_PIN] = false;
+    pinStateMap[D5_PIN] = false;
+    pinStateMap[D6_PIN] = false;
+    pinStateMap[D7_PIN] = false;
+    pinStateMap[D8_PIN] = false;
+}
+
+
+void setupIPConfig(){
+    IPAddress ipv4(192, 168, 1, 180);
+    IPAddress standardGateway(192, 168, 1, 1);
+    IPAddress subnet(255, 255, 255, 0);
+    IPAddress primaryDNS(1, 1, 1, 1);
+    IPAddress secondaryDNS(8, 8, 8, 8);
+
+    if (!WiFi.config(ipv4, standardGateway, subnet, primaryDNS, secondaryDNS)) {
+        Serial.println("[WARN] IP Adresse konnte nicht konfiguriert werden.");
+        return;
+    }
+
+    Serial.println("[SETUP] IP Adresse erfolgreich konfiguriert.");
+}
+
+
+void setupFilesystem(){
+    if (!LittleFS.begin()) {
+        Serial.println("[WARN] Filesystem konnte nicht erstellt werden.");
+        return;
+    }
+
+    Serial.println("[SETUP] Filesystem erfolgreich erstellt.");
+}
 
 
 void connectToWiFi() {
     WiFi.mode(WIFI_STA);
     WiFi.begin("Crafti-WLAN", "BS-IB96:-)");
-    Serial.println("\nConnecting to WiFi ");
+    Serial.println("[SETUP] Verbindung zum WLAN wird aufgebaut.");
+
     while (WiFi.status() != WL_CONNECTED) {
         Serial.print('.');
         delay(1000);
     }
-    if (WiFi.status() == WL_CONNECTED) {
-        Serial.println("Connected!");
-    } else {
-        Serial.println("Connection timeout!");
-    }
-}
 
-void initFS(){
-    if (!LittleFS.begin()) {
-        Serial.println("An error...");
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("[WARN] Verbindung zum WLAN fehlgeschlagen.");
+        return;
     }
-    Serial.println("LittleFS mounted successfully lol");
+
+    Serial.println("[SETUP]  Verbindung zum WLAN hergestellt.");
 }
 
 
-void handleGetAllDataRequest(AsyncWebServerRequest *request) {
+void handleHttpDataRequest(AsyncWebServerRequest *request) {
 
+    StaticJsonDocument<50> jsonDocument;
+    DeserializationError error = deserializeJson(jsonDocument, request->arg("data"));
+
+    if (error) {
+        Serial.println("[WARN] JSON PARSING FAILED.");
+        Serial.println(String("[WARN] ERROR: ").concat(error.c_str()));
+        return;
+    }
+
+    int key = jsonDocument["key"];
+    pinStateMap[key] = !pinStateMap[key];
+
+    if (pinStateMap[key]) digitalWrite(key, HIGH);
+    else digitalWrite(key, LOW);
+
+    request->send(200, "text/plain", "[DEBUG] Request 'UpdatePinState' handled successfully.");
+}
+
+
+void handleHttpAllDataRequest(AsyncWebServerRequest *request) {
     DynamicJsonDocument jsonDocument(300);
 
     JsonArray keyArray = jsonDocument.createNestedArray("keys");
@@ -62,102 +121,38 @@ void handleGetAllDataRequest(AsyncWebServerRequest *request) {
     keyArray.add(D8_PIN);
 
     JsonArray valueArray = jsonDocument.createNestedArray("values");
-    valueArray.add(pinMap[D1_PIN]);
-    valueArray.add(pinMap[D2_PIN]);
-    valueArray.add(pinMap[D3_PIN]);
-    valueArray.add(pinMap[D4_PIN]);
-    valueArray.add(pinMap[D5_PIN]);
-    valueArray.add(pinMap[D6_PIN]);
-    valueArray.add(pinMap[D7_PIN]);
-    valueArray.add(pinMap[D8_PIN]);
+    valueArray.add(pinStateMap[D1_PIN]);
+    valueArray.add(pinStateMap[D2_PIN]);
+    valueArray.add(pinStateMap[D3_PIN]);
+    valueArray.add(pinStateMap[D4_PIN]);
+    valueArray.add(pinStateMap[D5_PIN]);
+    valueArray.add(pinStateMap[D6_PIN]);
+    valueArray.add(pinStateMap[D7_PIN]);
+    valueArray.add(pinStateMap[D8_PIN]);
 
-    Serial.println(jsonDocument.memoryUsage());
+    Serial.println(jsonDocument.memoryUsage()); //TODO remove
     String jsonString;
     serializeJson(jsonDocument, jsonString);
-
-
 
     AsyncWebServerResponse *response = request->beginResponse(200, "application/json", jsonString);
     request->send(response);
 }
 
-void handleAllOff(AsyncWebServerRequest( *request)){
 
-    for (auto it = pinMap.begin(); it != pinMap.end(); ++it) {
+void handleHttpAllOffRequest(AsyncWebServerRequest(*request)){
+
+    for (auto it = pinStateMap.begin(); it != pinStateMap.end(); ++it) {
         int key = it->first;
-        pinMap[key] = false;
+        pinStateMap[key] = false;
         digitalWrite(key, LOW);
     }
 
-
-    request->send(200, "text/plain", "ist geschehen");
+    request->send(200, "text/plain", "[DEBUG] Request 'AllOff' handled successfully.");
 }
 
-void handleHTTPRequest(AsyncWebServerRequest *request) {
 
-    StaticJsonDocument<50> jsonDocument;
-    DeserializationError error = deserializeJson(jsonDocument, request->arg("data"));
-
-    if (error) {
-        Serial.print("parsing JSON failed: ");
-        Serial.println(error.c_str());
-        return;
-    }
-
-    int key = jsonDocument["key"];
-
-    pinMap[key] = !pinMap[key];
-    if (pinMap[key]) digitalWrite(key, HIGH);
-    else digitalWrite(key, LOW);
-
-
-    request->send(200, "text/plain", "Finished");
-}
-
-void setup() {
-    pinMap[D1_PIN] = false;
-    pinMap[D2_PIN] = false;
-    pinMap[D3_PIN] = false;
-    pinMap[D4_PIN] = false;
-    pinMap[D5_PIN] = false;
-    pinMap[D6_PIN] = false;
-    pinMap[D7_PIN] = false;
-    pinMap[D8_PIN] = false;
-
-
-    // setup globals
-    pinMode(D1_PIN, OUTPUT);
-    pinMode(D2_PIN, OUTPUT);
-    pinMode(D3_PIN, OUTPUT);
-    pinMode(D4_PIN, OUTPUT);
-    pinMode(D5_PIN, OUTPUT);
-    pinMode(D6_PIN, OUTPUT);
-    pinMode(D7_PIN, OUTPUT);
-    pinMode(D8_PIN, OUTPUT);
-
-    digitalWrite(D1_PIN, LOW);
-    digitalWrite(D2_PIN, LOW);
-    digitalWrite(D3_PIN, LOW);
-    digitalWrite(D4_PIN, LOW);
-    digitalWrite(D5_PIN, LOW);
-    digitalWrite(D6_PIN, LOW);
-    digitalWrite(D7_PIN, LOW);
-    digitalWrite(D8_PIN, LOW);
-
-    if (!WiFi.config(ipv4, standardGateway, subnet, primaryDNS, secondaryDNS)) Serial.println("ging nicht");
-
-
-    Serial.begin(115200); // start serial
-    initFS();
-
-
-
-    connectToWiFi();
-
-
-
-
-    //server static html
+void setupWebservers(){
+    //Server main Website (html)
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
         request->send(LittleFS, "index.html", "text/html");
     });
@@ -165,23 +160,27 @@ void setup() {
     //Serve static files (css, js)
     server.serveStatic("/", LittleFS, "/");
 
-
-    //handle all incoming httprequests on /data
-    server.on("/data", handleHTTPRequest);
-
-    server.on("/getAllData", handleGetAllDataRequest);
-
-    server.on("/allOff", handleAllOff);
+    //subpages for http requests
+    server.on("/data", handleHttpDataRequest);
+    server.on("/getAllData", handleHttpAllDataRequest);
+    server.on("/allOff", handleHttpAllOffRequest);
 
     server.begin();
-    Serial.println("Webserver started!");
+    Serial.println("[SETUP] Webserver gestartet.");
+}
 
 
-    Serial.println(WiFi.localIP());
+void setup() {
+    Serial.begin(115200);
+    setupPins();
+    setupIPConfig();
+    setupFilesystem();
+    connectToWiFi();
+    setupWebservers();
 }
 
 
 void loop() {
-    delay(1000);
-    if (WiFi.status() != WL_CONNECTED) connectToWiFi(); // if not connected to wifi, reconnect
+    delay(10000);
+    if (WiFi.status() != WL_CONNECTED) connectToWiFi();
 }
